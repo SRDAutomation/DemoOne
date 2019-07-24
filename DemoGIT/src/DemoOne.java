e53080c902039f43ebd7129decc04f41f83f4413
dassou12 <dassou12@GBKTFDC001X0075.kits.kfplc.com>
2019-07-24 18:22:19 +0530

    hgvhhhv

--- DemoGIT/src/DemoOne.java
+++ DemoGIT/src/DemoOne.java
@@ -4,7 +4,7 @@ public class DemoOne {
 	public static void main(String args[]){
 		//System.out.println("This is Demo One");
 		System.out.println("Hi hi ho");
-		System.out.println("Hi hi yyiyy");
+		System.out.println("Hi hi yyiy");
 		System.out.println("HJhiuiiu");
 	}
 	
