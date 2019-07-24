ab057c85f2f4134f2dbe5fff48944bcf8a323143
dassou12 <dassou12@GBKTFDC001X0075.kits.kfplc.com>
2019-07-24 13:38:52 +0530

    cbvb

--- Demo/src/DemoOne.java
+++ Demo/src/DemoOne.java
@@ -2,7 +2,9 @@
 public class DemoOne {
 
 	public static void main(String args[]){
-		System.out.println("This is Demo One");
+		//System.out.println("This is Demo One");
+		System.out.println("Hi");
+		System.out.println("HJh");
 	}
 	
 }
