7b3d2cac11b7178fe6ffdac62fb4f29e188bd1ce
dassou12 <dassou12@GBKTFDC001X0075.kits.kfplc.com>
2019-07-24 18:19:42 +0530

    Comited

--- DemoGIT/src/DemoOne.java
+++ DemoGIT/src/DemoOne.java
@@ -4,7 +4,7 @@ public class DemoOne {
 	public static void main(String args[]){
 		//System.out.println("This is Demo One");
 		System.out.println("Hi hi ho");
-		System.out.println("Hi hi yyuy");
+		System.out.println("Hi hi yyiyy");
 		System.out.println("HJhiuiiu");
 	}
 	
