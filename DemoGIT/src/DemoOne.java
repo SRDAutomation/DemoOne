d46e56fe5784f17ce4e213cb13fa6565df152917
dassou12 <dassou12@GBKTFDC001X0075.kits.kfplc.com>
2019-07-24 14:54:39 +0530

    New Comit

---
+++ DemoGIT/src/DemoOne.java
@@ -0,0 +1,10 @@
+
+public class DemoOne {
+
+	public static void main(String args[]){
+		//System.out.println("This is Demo One");
+		System.out.println("Hi hi ho");
+		System.out.println("HJhiuiiu");
+	}
+	
+}
